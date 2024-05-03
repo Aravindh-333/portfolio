51cb6592bc40aa806c08f5f1ee4db62507eeb855
ARAVINDH A <129385693+Aravindh-333@users.noreply.github.com>
2024-05-03 09:35:58 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,13 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">ARAVINDH A </span>
+            from <span className="purple"> DHARMAPURI, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently Currently pursuing a Computer Science Engineering
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed by HSC in Senthil Matric hr sec School.
+            Dharmapuri.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -24,7 +24,7 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Seeing Adventure Movies
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -32,9 +32,9 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "LEARN FROM ANYONE,BUT FOLLOW NO ONE!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">ARAVINDH A</footer>
         </blockquote>
       </Card.Body>
     </Card>
