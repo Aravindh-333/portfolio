461578ad46422b1313971d2e870f1858779f35ed
ARAVINDH A <129385693+Aravindh-333@users.noreply.github.com>
2025-09-16 11:09:46 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,32 +1,68 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  DiJavascript1,
-  DiReact,
   DiPython,
   DiGit,
-  DiJava,
 } from "react-icons/di";
+import {
+  SiMysql,
+  SiPowerbi,
+  SiPandas,
+  SiJupyter,
+  SiTableau,
+  SiScikitlearn,
+  SiNumpy
+} from "react-icons/si";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
+      
+      {/* Python Icon */}
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
+
+      {/* Git / GitHub Icon */}
       <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
+        <DiGit />
       </Col>
-       
+
+      {/* MySQL Icon */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMysql />
+      </Col>
+
+      {/* Power BI Icon */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPowerbi />
+      </Col>
+
+      {/* Pandas Icon */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPandas />
+      </Col>
+
+      {/* Jupyter Notebook Icon */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJupyter />
+      </Col>
+
+      {/* Tableau Icon */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTableau />
+      </Col>
+
+      {/* Machine Learning Icon */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiScikitlearn />
+      </Col>
+
+      {/* NumPy Icon */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNumpy />
+      </Col>
+
     </Row>
   );
 }
