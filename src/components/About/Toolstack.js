ac49a8de615f85b7889770544daad8f07bc8b37f
ARAVINDH A <129385693+Aravindh-333@users.noreply.github.com>
2025-09-16 11:19:05 +0530

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -2,16 +2,66 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
+  SiMicrosoftpowerpoint,
+  SiMicrosoftteams,
+  SiMicrosoftword,
+  SiMicrosoftexcel,
+  SiJupyter,
+  SiAnaconda,
+  SiLinux,
+  SiOracle
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+
+      {/* VS Code */}
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
-    </Row>
 
+      {/* Power BI (using PowerPoint as closest available) */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMicrosoftpowerpoint />
+      </Col>
+
+      {/* Microsoft Teams */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMicrosoftteams />
+      </Col>
+
+      {/* Microsoft Word */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMicrosoftword />
+      </Col>
+
+      {/* Microsoft Excel */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMicrosoftexcel />
+      </Col>
+
+      {/* Jupyter Notebook */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJupyter />
+      </Col>
+
+      {/* Anaconda */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAnaconda />
+      </Col>
+
+      {/* Linux */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLinux />
+      </Col>
+
+      {/* Oracle Virtual Machine */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiOracle />
+      </Col>
+
+    </Row>
   );
 }
 
